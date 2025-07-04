b6d6072a6301bcaa97bc0dd1b7f6cb38af357914
unknown <suppims900@mail.com>
2025-07-04 16:46:12 +0530

    Authmiddlewares and cartcontroller

---
+++ middlewares/authMiddlewares.js
@@ -0,0 +1,31 @@
+//Middleware for checking logged-in user
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const isAuthenticated = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.userId).select("-password");
+    next();
+  } catch (error) {
+    console.error("Authentication error:", error);
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+// Middleware for checking admin role
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    return next();
+  }
+  return res.status(403).json({ message: "Access denied: Admins only" });
+};
+
+module.exports = {
+  isAuthenticated,
+  isAdmin,
+};
